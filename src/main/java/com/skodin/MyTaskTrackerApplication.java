7343b0be264c4bc81c49c3d9685e51e9e0a5d58b
Nikita Skodin <skodin36@gmail.com>
2023-10-18 12:31:39 +0300

    commit

---
+++ src/main/java/com/skodin/MyTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skodin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTaskTrackerApplication.class, args);
+    }
+
+}
