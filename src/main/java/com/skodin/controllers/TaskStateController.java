fd87556e49fdb4876ac1de405ec68b246ee1c317
Nikita Skodin <skodin36@gmail.com>
2023-10-19 22:09:09 +0300

    DeleteProject method was added

--- src/main/java/com/skodin/controllers/TaskStateController.java
+++ src/main/java/com/skodin/controllers/TaskStateController.java
@@ -1,16 +1,44 @@
 package com.skodin.controllers;
 
+import com.skodin.DTO.TaskStateDTO;
+import com.skodin.models.TaskStateEntity;
+import com.skodin.services.ProjectService;
+import com.skodin.util.ModelMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * проверка на существование проекта с id
+ * происходит при конвертации из DTO
+ */
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/projects/{project_id}/task-states")
 public class TaskStateController {
-    // TODO: 019 валидировать id проекта при входе в DTO
 
+    ProjectService projectService;
+
+    public static final String GET_ALL_TASK_STATES = "";
+
+    @GetMapping(GET_ALL_TASK_STATES)
+    public ResponseEntity<List<TaskStateDTO>> getAllTaskStates(@PathVariable("project_id") Long id){
+
+        List<TaskStateEntity> taskStateEntities = projectService.findById(id).getTaskStateEntities();
+
+        return ResponseEntity
+                .ok()
+                .body(taskStateEntities.stream()
+                        .map(ModelMapper::getTaskStateDTO).collect(Collectors.toList()));
+
+    }
 }
