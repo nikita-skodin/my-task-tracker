1af1f60cff8af8c74121b9bc79947f750ccadcdb
Nikita Skodin <skodin36@gmail.com>
2023-11-05 13:59:05 +0300

    security fix

--- src/main/java/com/skodin/services/JwtService.java
+++ src/main/java/com/skodin/services/JwtService.java
@@ -1,39 +1,36 @@
 package com.skodin.services;
 
 import com.skodin.models.UserEntity;
+import com.skodin.util.auth.JWTProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 @Service
-//@PropertySource("file:/my-task-tracker/.env")
+@RequiredArgsConstructor
 public class JwtService {
 
-    // TODO: 001 вынести
-//    @Value("${SECRET}")
-    private static final String SECRET_KEY = "gSlrTZoP7iFZP6c1m9f6h1JwNP2X89+y0JHQ3EXo/hklG2euXqSMEbfpMqY0KaqH";
+    private final JWTProperties jwtProperties;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+
     public Long extractId(String token) {
 
-        if (token.startsWith("Bearer ")){
+        if (token.startsWith("Bearer ")) {
             token = token.substring(7);
         }
 
@@ -48,27 +45,27 @@ public class JwtService {
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
-        return  claimsResolver.apply(claims);
+        return claimsResolver.apply(claims);
     }
 
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         UserEntity entity = (UserEntity) userDetails;
         Map<String, String> extractClaims = Collections.singletonMap("id", entity.getId().toString());
         return generateToken(extractClaims, userDetails);
     }
 
-    public String generateToken(Map<String, String> extractClaims, UserDetails userDetails){
+    public String generateToken(Map<String, String> extractClaims, UserDetails userDetails) {
         return Jwts
                 .builder()
                 .setClaims(extractClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getAccess()))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public boolean isTokenValid(String token, UserDetailsService userDetailsService){
+    public boolean isTokenValid(String token, UserDetailsService userDetailsService) {
         try {
             String username = extractUsername(token);
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
@@ -87,7 +84,7 @@ public class JwtService {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    private Claims extractAllClaims(String token){
+    private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(getSignKey())
@@ -97,7 +94,7 @@ public class JwtService {
     }
 
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecret());
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
